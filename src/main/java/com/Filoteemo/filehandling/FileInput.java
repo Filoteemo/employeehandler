6d318896fdc45dce2409c02d8955d7d9fb8ed2d0
SindreOlsen <64730885+Filoteemo@users.noreply.github.com>
2021-04-27 15:34:36 +0200

    Revert "Filepath problem"
    This reverts commit e33d19713d02389052a66dc629a83acb2a194137.

--- src/main/java/com/Filoteemo/filehandling/FileInput.java
+++ src/main/java/com/Filoteemo/filehandling/FileInput.java
@@ -9,20 +9,20 @@ public class FileInput {
 	
 	public FileInput() {
 		
-//		// how to get filepath on mac: right click file, hold down option
-//		Path macFile = Paths.get("/Users/sindreolsen/Java/test.txt");
-//		String s = "Hei Sindre";
-//		byte[] data = s.getBytes();
-//		
-//		try {
-//			FileOutputStream output = new FileOutputStream(macFile.toString());
-//			output.write(data);
-//			output.close();
-//			System.out.println("Success");
-//		}
-//		catch(Exception e) {
-//			System.out.println(e);
-//		}
+		// how to get filepath on mac: right click file, hold down option
+		Path file = Paths.get("/Users/sindreolsen/Java/test.txt");
+		String s = "Hei Sindre";
+		byte[] data = s.getBytes();
+		
+		try {
+			FileOutputStream output = new FileOutputStream(file.toString());
+			output.write(data);
+			output.close();
+			System.out.println("Success");
+		}
+		catch(Exception e) {
+			System.out.println(e);
+		}
 		
 	}
 
